ccacb5221a5bac0a6145c0a4c3317a5a015e784e
Britowiski <132316224+Britowiski@users.noreply.github.com>
2024-04-12 17:24:50 -0300

    Add files via upload

---
+++ Interface_pagamentos/pagamentos.py
@@ -0,0 +1,41 @@
+insira_valor = int(input("Valor: "))
+
+try:
+    while(True):
+
+        formas_pagamento = input("Selecione a forma de pagamento: \n ")
+
+        if formas_pagamento == "Debito":
+
+            print(f" Insira ou aproxime o cartão.\n R$:{insira_valor},00")
+            break
+
+        elif formas_pagamento == "Credito":
+
+            print("Forma de pagamento")
+
+            pagamento_credito = input("Selecione\n A vista \n Parcelado\n")
+
+            if pagamento_credito == "A vista":
+
+                print(f"Insira ou aproxime o cartão:\n {insira_valor},00")
+
+                break
+
+            elif pagamento_credito == "Parcelado":
+
+                print("Quantidade de parcelas\n")
+
+                parcelas = int(input("Digite as parcelas\n"))
+
+                if parcelas == 2:
+                    
+                    print(f"1° Parcela {insira_valor/2}")
+                    print(f"2° Parcela {insira_valor/2}")
+                    break
+                elif parcelas == 3:
+                    print(f"1° Parcela {insira_valor}/3")
+                    print(f"2° Parcela {insira_valor}/3")
+                    print(f"3° Parcela {insira_valor}/3")
+except:
+    print("Não foi possivel efetuar a solicitação")
