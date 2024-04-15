1c6d3d958466f434e2d16f98e8d67a1337e4e41e
Britowiski <132316224+Britowiski@users.noreply.github.com>
2024-04-15 15:36:07 -0300

    Add files via upload
    Implemented new method to calculate the division of each payment that was chosen.

--- Interface_pagamentos/pagamentos.py
+++ Interface_pagamentos/pagamentos.py
@@ -1,42 +1,37 @@
-insira_valor = int(input("Valor: "))
+insira_valor = float(input("Valor: "))
 
 try:
     while(True):
 
-        formas_pagamento = input("Selecione a forma de pagamento: \n Debito\n Credito")
+        formas_pagamento = input("Selecione a forma de pagamento: \n (1) Debito \n (2) Credito \n ")
 
-        if formas_pagamento == "Debito":
+        if formas_pagamento == "Debito" or formas_pagamento == "1":
 
             print(f" Insira ou aproxime o cartão.\n R$:{insira_valor},00")
             break
 
-        elif formas_pagamento == "Credito":
+        elif formas_pagamento == "Credito" or formas_pagamento == "2":
 
-            print("Forma de pagamento")
 
-            pagamento_credito = input("Selecione\n A vista \n Parcelado\n")
+            pagamento_credito = input("Selecione\n (1) A vista \n (2) Parcelado\n ")
 
-            if pagamento_credito == "A vista":
+            if pagamento_credito == "A vista" or pagamento_credito == "1":
 
                 print(f"Insira ou aproxime o cartão:\n {insira_valor},00")
 
                 break
 
-            elif pagamento_credito == "Parcelado":
-
-                print("Quantidade de parcelas\n")
-
+            elif pagamento_credito == "Parcelado" or pagamento_credito == "2":
                 parcelas = int(input("Digite as parcelas\n"))
 
-                if parcelas == 2:
-                    
-                    print(f"1° Parcela {insira_valor/2}")
-                    print(f"2° Parcela {insira_valor/2}")
-                    break
-                elif parcelas == 3:
-                    print(f"1° Parcela {int(insira_valor/3)}")
-                    print(f"2° Parcela {int(insira_valor/3)}")
-                    print(f"3° Parcela {int(insira_valor/3)}")
-                    break
+            if parcelas >= 2 and parcelas <= 5:
+                print("Insira ou aproxime o cartão")
+                valor_parcela = insira_valor / parcelas 
+                for i in range(parcelas):
+                    print(f"{i+1}° Parcela: R${valor_parcela:.2f}")
+                break
+            else:
+                print("Número de parcelas inválido. Por favor, escolha um número de parcelas entre 2 e 5.")
+
 except:
     print("Não foi possivel efetuar a solicitação")
